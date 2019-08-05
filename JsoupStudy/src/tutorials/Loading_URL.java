d5598085c0c3677b5aa8783c5446ec5dfb2e25af
Anthony <Anthony@DESKTOP-DKS5AF2>
2019-08-05 01:57:32 -1000

    Update basic tutorials

--- JsoupStudy/src/tutorials/Loading_URL.java
+++ JsoupStudy/src/tutorials/Loading_URL.java
@@ -11,6 +11,9 @@ public class Loading_URL
 	public static void main(String[] args) throws IOException
 	{
 		String url = "https://www.basketball-reference.com/leagues/NBA_2019_ratings.html";
+		// Jsoup.connect(String uri) establishes a connect (via Connection object)
+		// .get() parses it into a Document object for traversal 
+
 		Document doc = Jsoup.connect(url).get();
 		Elements para = doc.getElementsByTag("tr");
 		
